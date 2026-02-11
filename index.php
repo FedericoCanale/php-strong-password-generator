b111a4953bb0e27605f86ab4eeab533348e8ef74
FedericoCanale <icocanale@gmail.com>
2026-02-11 09:56:51 +0100

    First milestone complete

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
-function generatePassword($length) {
+function generatePassword($length)
+{
     $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=';
     $password = '';
 
@@ -22,11 +23,13 @@ if (isset($_GET['length'])) {
 
 <!DOCTYPE html>
 <html lang="it">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Strong Password Generator</title>
 </head>
+
 <body>
     <h1>Strong Password Generator</h1>
 
@@ -41,4 +44,5 @@ if (isset($_GET['length'])) {
         <p><?php echo $password; ?></p>
     <?php endif; ?>
 </body>
+
 </html>
